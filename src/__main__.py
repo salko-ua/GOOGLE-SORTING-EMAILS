5eb2f7208eded173c049e45ea833ac701c60a2d2
salko-ua <chips69jamil69game@gmail.com>
2024-02-24 23:37:39 +0200

    poetry lock

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,22 @@
 import os.path
+from pprint import pprint
 
+import google.auth
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://www.googleapis.com/auth/gmail.send",
+    'https://www.googleapis.com/auth/gmail.compose',
+    'https://www.googleapis.com/auth/gmail.insert',
+    'https://www.googleapis.com/auth/gmail.modify',
+    'https://www.googleapis.com/auth/gmail.metadata',
+
+]
 
 
 def main():
@@ -14,13 +25,18 @@ def main():
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
     try:
-        # Call the Gmail API
-        service = build("gmail", "v1", credentials=creds)
-        results = service.users().labels().list(userId="me").execute()
-        print(results)
+        service = build('gmail', 'v1', credentials=creds)
+
+        result = service.users().labels().list(userId='me').execute()
+        labels = result.get('labels', [])
+        for label in labels:
+            print(label["name"])
+
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
         print(f"An error occurred: {error}")
+
+
 
 
 if __name__ == "__main__":
